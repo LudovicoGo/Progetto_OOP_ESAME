d1ac254be3ff330d7d9c1e772bf5fae9eefc0607
ChristianCurzi <91216278+ChristianCurzi@users.noreply.github.com>
2021-12-20 10:22:35 +0100

    Rinominata calsse variance in statistics e modifiata;

--- ProgettoEsame/src/main/java/com/progetto/ProgettoEsame/filterAndStats/Statistics.java
+++ ProgettoEsame/src/main/java/com/progetto/ProgettoEsame/filterAndStats/Statistics.java
@@ -4,12 +4,14 @@ import java.util.Vector;
 
 // import com.progetto.ProgettoEsame.model.VisibilityStatsModel;
 
-public class Variance {
+public class Statistics {
 
     private double variance; // variance = sum/(numberOfSamples - 1);
     private double average; // media dei campioni
     private int numberOfSamples; // numero di campioni
     private double sum; // sum = sommatoria di (termInDataSet - meanVis)^2
+    private long maxValue; // valore massimo registrato
+    private long minValue; // valore minimo registrato
 
     public double getVariance() {
         return variance;
@@ -47,6 +49,32 @@ public class Variance {
 
         this.variance = variance;
         return variance;
+    }
+
+    public long maxValue (Vector<Long> getData){
+
+        long currentData = 0;
+
+        for (long termInDataSet : getData) {
+            if (currentData < termInDataSet)
+                currentData = termInDataSet;
+        }
+
+        this.maxValue = currentData;
+        return maxValue;
+    }
+
+    public long minValue (Vector<Long> getData){
+
+        long currentData = getData.get(0);
+
+        for (long termInDataSet : getData) {
+            if (currentData > termInDataSet)
+                currentData = termInDataSet;
+        }
+
+        this.maxValue = currentData;
+        return maxValue;
     }
 
 }
