d2ff68ef5bbda84db9473e8a1b74bd06d8c4ec2f
ChristianCurzi <91216278+ChristianCurzi@users.noreply.github.com>
2021-12-22 15:55:13 +0100

    Aggiunto javadoc alla classe CityForStatsModel

--- ProgettoEsame/src/main/java/com/progetto/ProgettoEsame/model/CityForStatsModel.java
+++ ProgettoEsame/src/main/java/com/progetto/ProgettoEsame/model/CityForStatsModel.java
@@ -1,16 +1,38 @@
 package com.progetto.ProgettoEsame.model;
 
+/** Classe che contiene il nome delle città per le statistiche.
+ * @author Gorgoglione Ludovico
+ * @author Curzi Christian
+ */
+
 public class CityForStatsModel {
 
+    /**
+     * Variabile che rappresenta il nome della città
+     */
     String cityName;
+
+    /** Costruttore dell'oggetto.
+     * @param cityName             Nome della città.
+     */
 
     public CityForStatsModel(String cityName) {
         this.cityName = cityName;
     }
 
+    /**
+     * Metodo che restituisce il nome della città.
+     * @return cityName
+     */
+
     public String getCityName() {
         return cityName;
     }
+
+    /**
+     * Metodo che setta il nome della città.
+     * @param cityName
+     */
 
     public void setCityName(String cityName) {
         this.cityName = cityName;
