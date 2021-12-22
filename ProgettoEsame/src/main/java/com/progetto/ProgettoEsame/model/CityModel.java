6ca8ca10d43d9989e0621e7c9e0eaab2d2fb392d
ChristianCurzi <91216278+ChristianCurzi@users.noreply.github.com>
2021-12-22 12:22:36 +0100

    Aggiunto javadoc alla classe CityModel

--- ProgettoEsame/src/main/java/com/progetto/ProgettoEsame/model/CityModel.java
+++ ProgettoEsame/src/main/java/com/progetto/ProgettoEsame/model/CityModel.java
@@ -6,30 +6,75 @@ public class CityModel extends GeolocationModel {
     private long cityId;
     private String country;
 
+    /** Costruttore di default dell'oggetto.
+     *
+     */
+
     public CityModel() {
         super();
     }
 
+    /**
+     * Metodo che restituisce il nome della città.
+     * @return cityName
+     */
+
     public String getCityName() {
         return cityName;
     }
+
+    /**
+     * Metodo che setta il nome della città.
+     * @param name
+     */
+
     public void setCityName(String name) {
         this.cityName = name;
     }
 
+    /**
+     * Metodo che restituisce l'ID della città.
+     * @return cityId
+     */
+
     public long getCityId() {
         return cityId;
     }
+
+    /**
+     * Metodo che setta l'ID della città.
+     * @param id
+     */
+
     public void setCityId(long id) {
         this.cityId = id;
     }
 
+    /**
+     * Metodo che restituisce la nazione in cui si trova la città.
+     * @return country
+     */
+
     public String getCountry() {
         return country;
     }
+
+    /**
+     * Metodo che setta la nazione in cui si trova la città.
+     * @param country
+     */
+
     public void setCountry(String country) {
         this.country = country;
     }
+
+    /** Costruttore dell'oggetto.
+     * @param name         Nome della città.
+     * @param cityId       ID della città.
+     * @param country      Nazione in cui si trova la città.
+     * @param latitude     Latitudine della città.
+     * @param longitude    Longitudine della città.
+     */
 
     public CityModel(String name, long cityId, String country, double latitude, double longitude) {
         super(latitude, longitude);
@@ -37,4 +82,5 @@ public class CityModel extends GeolocationModel {
         this.cityId = cityId;
         this.country = country;
     }
+
 }
